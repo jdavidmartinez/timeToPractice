3c054ab6a6505b9f25ba477ee191308d7f1152e9
DaviD D <jdavid.martinez@gmail.com>
2021-06-06 15:30:06 -0500

    Video94

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUser from '../src/components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UserList users={[]} />
     </div>
   );
 }
